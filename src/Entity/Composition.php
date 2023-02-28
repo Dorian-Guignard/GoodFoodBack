464eb439494c98182ea1792bec2076b4275b3d39
Dorian-Guignard <web.guidor@gmail.com>
2023-02-28 09:46:56 +0100

    compositionOK

--- src/Entity/Composition.php
+++ src/Entity/Composition.php
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Constraints;
 
 /**
  * @ORM\Entity(repositoryClass=CompositionRepository::class)
- * 
+ *
  */
 class Composition
 {
@@ -26,82 +26,65 @@ class Composition
      * @Groups({"compositions_get_collection", "virtues_get_item"})
      */
     private $id;
-
     /**
      * @ORM\ManyToOne(targetEntity=Recipe::class, inversedBy="compositions")
      * @ORM\JoinColumn(nullable=false)
      * @Groups({"compositions_get_collection", "compositions_get_item"})
      */
     private $recipe;
-
     /**
      * @ORM\ManyToOne(targetEntity=Food::class, inversedBy="compositions")
      * @Groups({"compositions_get_collection", "compositions_get_item"})
      */
     private $food;
-
     /**
      * @ORM\Column(type="string", length=20, nullable=true)
      * @Groups({"compositions_get_collection", "compositions_get_item"})
      */
     private $unity;
-
     /**
-     * @ORM\Column(type="integer",nullable=true)
+     * @ORM\Column(type="string", length=10, nullable=true)
      * @Groups({"compositions_get_collection", "compositions_get_item"})
      */
     private $quantity;
-
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getRecipe(): ?Recipe
     {
         return $this->recipe;
     }
-
     public function setRecipe(?Recipe $recipe): self
     {
         $this->recipe = $recipe;
-
         return $this;
     }
-
     public function getFood(): ?Food
     {
         return $this->food;
     }
-
     public function setFood(?Food $food): self
     {
         $this->food = $food;
-
         return $this;
     }
-
     public function getUnity(): ?string
     {
         return $this->unity;
     }
-
     public function setUnity(?string $unity): self
     {
         $this->unity = $unity;
-
         return $this;
     }
-
-    public function getQuantity(): ?int
+    public function getQuantity(): ?string
     {
         return $this->quantity;
     }
-
-    public function setQuantity(int $quantity): self
+    public function setQuantity(?string $quantity): self
     {
         $this->quantity = $quantity;
-
         return $this;
     }
 }
