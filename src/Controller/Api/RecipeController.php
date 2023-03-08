21579dd88c85f4ad33416b38f8ccdc2bc9c653c5
Dorian-Guignard <web.guidor@gmail.com>
2023-03-08 08:42:02 +0100

    wipImageFileAPI

--- src/Controller/Api/RecipeController.php
+++ src/Controller/Api/RecipeController.php
@@ -12,14 +12,17 @@ use App\Repository\RecipeRepository;
 use App\Repository\VirtueRepository;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
 
 
@@ -137,65 +140,97 @@ class RecipeController extends AbstractController
      * 
      * @Route("/api/recipes", name="app_api_post_recipes_item", methods={"POST"})
      */
-    public function create(FoodRepository $foodRepository, Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository, VirtueRepository $virtueRepository, UserRepository $userRepository)
-    {
-
+    public function create(
+        Request $request,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator,
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger,
+        ManagerRegistry $doctrine
+    ) {
         $user = $this->getUser();
+
         $jsonContent = $request->getContent();
-        $recipe = $serializer->deserialize($jsonContent, Recipe::class, "json", [AbstractNormalizer::IGNORED_ATTRIBUTES => ['category', 'virtue', 'compositions'], AbstractObjectNormalizer::DEEP_OBJECT_TO_POPULATE => true,]);
+
+        $recipe = $serializer->deserialize(
+            $jsonContent,
+            Recipe::class,
+            "json",
+            [AbstractNormalizer::IGNORED_ATTRIBUTES =>
+            ['category', 'virtue', 'compositions'], AbstractObjectNormalizer::DEEP_OBJECT_TO_POPULATE => true,]
+        );
+
+
         $recipe->setUser($user);
 
         $decodedContent = json_decode($jsonContent, true);
 
         // On set manuellement category et virtue à partir de leurs id
-        $recipe->setCategory($categoryRepository->find($decodedContent['category']))
-            ->setVirtue($virtueRepository->find($decodedContent['virtue']));
-
+        $recipe->setCategory($doctrine->getRepository(Category::class)->find($decodedContent['category']))
+            ->setVirtue($doctrine->getRepository(Virtue::class)->find($decodedContent['virtue']));
 
         // On crée chaque composition et on l'ajoute à recipe
         foreach ($decodedContent['compositions'] as $compositionData) {
             $composition = new Composition();
-            $composition->setFood($foodRepository->find($compositionData['food']))
-                ->setUnity($compositionData['unity'])->setQuantity($compositionData['quantity']);
+            $composition->setFood($doctrine->getRepository(Food::class)->find($compositionData['food']))
+                ->setUnity($compositionData['unity'])->setQuantity($compositionData['quantité']);
             $recipe->addComposition($composition);
+        }
+
+        // Gestion de l'image
+        $imageFile = $request->files->get('nameImage');
+        if ($imageFile) {
+            $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+            // this is needed to safely include the file name as part of the URL
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+            // Move the file to the directory where images are stored
+            try {
+                $imageFile->move(
+                    $this->getParameter('new_item_directory'),
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                // ... handle exception if something happens during file upload
+                return $this->json(['error' => 'votre telechargement a échoué'], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            // updates the 'nameImage' property to store the image file name
+            // instead of its contents
+            $recipe->setNameImage(
+                $this->getParameter('new_item_directory') . '/' . $newFilename
+            );
         }
 
         $errors = $validator->validate($recipe);
 
-        $errorsList = [];
         if (count($errors) > 0) {
-
+            $errorsList = [];
             foreach ($errors as $error) {
-
                 $errorsList[$error->getPropertyPath()][] = $error->getMessage();
             }
-
             return $this->json($errorsList, Response::HTTP_UNPROCESSABLE_ENTITY);
         }
-        /* var_dump($recipe); */
 
-        // 5. persist et flush
-
+        // Persist and flush the recipe
         $entityManager->persist($recipe);
         $entityManager->flush();
 
-
         return $this->json(
-
             ['recipe' => $recipe],
-
             Response::HTTP_CREATED,
-
             [
                 'Location' => $this->generateUrl(
                     'app_api_recipes_get_item',
                     ['id' => $recipe->getId()]
                 )
             ],
-
             ['groups' => 'recipes_get_item']
         );
     }
+
+
     /**
      * Delete recipe
      * 
